c4630ca1b0bedb95f20dfeb4bffdc5f50937a953
Stefan Espant <stefan.van.der.kort@hva.nl>
2023-09-04 22:33:55 +0200

    v1.0
    Wum geluidsfragment verwerkt

---
+++ src/lib/components/landing/Heading.svelte
@@ -0,0 +1,27 @@
+<section>
+    <h1>Goedemorgen <span>Siem</span></h1>
+    <p>Wat schrijf je vandaag op?</p>
+</section>
+
+<style>
+    section{
+        width: 90%;
+        margin: 2rem auto;
+    }
+
+    h1{
+        font-size: 1.5rem;
+        font-family: 'Inter SemiBold';
+        margin-bottom: 0.5rem;
+    }
+
+    span{
+        color: var(--light-secondary);
+    }
+
+    p{
+        font-size: 0.9rem;
+        font-family: 'Inter Regular';
+        color: var(--light-subheading-color);
+    }
+</style>
