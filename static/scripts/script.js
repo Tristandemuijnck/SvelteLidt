0bb4ff5ff4781a5965afb6c0d0c0ac95b0b3c428
Stefan Espant <stefan.van.der.kort@hva.nl>
2023-09-12 11:44:59 +0200

    v1.0.2
    progressive enhancement fixes for desktop nav

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,5 +1,7 @@
+document.documentElement.classList.add("javascriptEnabled");
+
 const desktopNav = document.querySelector('nav')
-const collapseButton = document.querySelector('button')
+const collapseButton = document.querySelector('.desktop-nav-button')
 
 collapseButton.addEventListener('click', () => {
     desktopNav.classList.toggle('to-the-left')
