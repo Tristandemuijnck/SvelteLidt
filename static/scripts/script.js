5235006024c5c56df5e32829252a4587422f1133
Stefan Espant <stefan.van.der.kort@hva.nl>
2023-09-12 13:03:56 +0200

    v1.0.3
    mobile menu first version

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,8 +1,19 @@
 document.documentElement.classList.add("javascriptEnabled");
 
+
+// Desktop collapse navigation enhancement
+
 const desktopNav = document.querySelector('nav')
 const collapseButton = document.querySelector('.desktop-nav-button')
 
 collapseButton.addEventListener('click', () => {
     desktopNav.classList.toggle('to-the-left')
+});
+
+// Wum soundeffect
+const wumAudio = new Audio('https://cdn.discordapp.com/attachments/1148257851664511019/1148351815784616008/wum-v2.mp3');
+
+window.addEventListener('load', function () {
+    const audio = wumAudio;
+    audio.play();
 });
