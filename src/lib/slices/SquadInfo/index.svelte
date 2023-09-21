471504ac5d97a13b4065472ca6ba20a0e41ba330
Stefan Espant <stefan.van.der.kort@hva.nl>
2023-09-21 17:05:32 +0200

    v2.0
    All members shown, reveal animation added on articles

--- src/lib/slices/SquadInfo/index.svelte
+++ src/lib/slices/SquadInfo/index.svelte
@@ -1,15 +1,130 @@
 <script>
-	/** @type {import("@prismicio/client").Content.SquadInfoSlice} */
 	export let slice;
+
+	// Sorteer de items op alfabetische volgorde op basis van de voornaam
+	const sortedItems = slice.items.slice().sort((a, b) => {
+		const nameA = a.member.data.firstname.toLowerCase();
+		const nameB = b.member.data.firstname.toLowerCase();
+		return nameA.localeCompare(nameB);
+	});
 </script>
 
-<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-	{#each slice.items as item, i}
-		<p>{item.member.data.link.url}</p>
+<section
+	class="section-members"
+	data-slice-type={slice.slice_type}
+	data-slice-variation={slice.variation}
+>
+	{#each sortedItems as item, i}
+		<article class="revealing-member">
+			<img
+				src={item.member.data.image.url}
+				alt="Foto van {item.member.data.firstname}"
+				width="320"
+				height="320"
+			/>
+			<h2>{item.member.data.firstname} {item.member.data.lastname}</h2>
+			<a href={item.member.data.link}>Bezoek mijn visitekaartje</a>
+		</article>
 	{/each}
 </section>
 
-
-<pre style="margin-left: 5rem;">
+<!-- <pre style="margin-left: 18rem;">
 	{JSON.stringify(slice, null, 2)}
-</pre>
+</pre> -->
+
+<style>
+	section {
+		margin: 5rem auto 5rem;
+		display: flex;
+		gap: var(--unit-default);
+		padding: var(--unit-default);
+		flex-wrap: wrap;
+	}
+
+	.section-members .to-the-left {
+		margin: auto;
+		margin-top: 0;
+		margin-left: 0rem;
+	}
+
+	article {
+		width: 20rem;
+		background-color: var(--light-primary);
+		padding: var(--unit-default);
+		padding-bottom: 3rem;
+		border-radius: var(--unit-default);
+		margin: auto;
+		position: relative;
+	}
+
+	img {
+		background-color: var(--light-accent);
+		width: 100%;
+		aspect-ratio: 2/3;
+		object-fit: cover;
+		object-position: top;
+		border-radius: var(--unit-small);
+		margin-bottom: var(--unit-default);
+	}
+
+	a {
+		color: var(--light-secondary);
+		transition: 0.2s ease-in-out;
+		transform: rotateZ(0deg);
+		position: absolute;
+	}
+
+	a:is(:hover, :focus) {
+		color: var(--light-accent);
+		transform: rotateZ(-8deg);
+	}
+
+	@media (min-width: 48rem) {
+		section {
+			width: 72vw;
+			margin-top: 0rem;
+			margin-left: 18rem;
+		}
+
+		article {
+			margin: 0;
+		}
+	}
+
+	@media (min-width: 90rem) {
+		section {
+			width: 64rem;
+		}
+	}
+
+	/* Typografie */
+
+	h2 {
+		margin-bottom: var(--unit-medium);
+	}
+
+	/* Keyframe Reveal effect */
+		.revealing-member {
+			/* Create View Timeline */
+			view-timeline-name: --revealing-image;
+			view-timeline-axis: block;
+
+			/* Attach animation, linked to the  View Timeline */
+			animation: linear reveal both;
+			animation-timeline: --revealing-image;
+
+			/* Tweak range when effect should run*/
+			animation-range: entry 10% cover 50%;
+		}
+
+		@keyframes reveal {
+			from {
+				opacity: 0;
+				clip-path: inset(45% 20% 45% 20%);
+			}
+			to {
+				opacity: 1;
+				clip-path: inset(0% 0% 0% 0%);
+			}
+		}
+</style>
--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,8 +1,10 @@
 document.documentElement.classList.add("javascriptEnabled");
 
+const sectionMembers = document.querySelector('.section-members')
 const desktopNav = document.querySelector('.desktop-nav')
 const collapseButton = document.querySelector('.desktop-nav-button')
 
 collapseButton.addEventListener('click', () => {
     desktopNav.classList.toggle('to-the-left')
+    sectionMembers.classList.toggle('to-the-left')
 });
